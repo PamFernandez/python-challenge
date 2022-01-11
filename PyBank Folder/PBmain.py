a6538a0cf7e9c596ceac4cc0cca1abfd48242eff
Pam Fernandez <ppossen@gmail.com>
2022-01-11 11:27:20 -0600

    1

--- PyBank Folder/PBmain.py
+++ PyBank Folder/PBmain.py
@@ -1,1 +1,36 @@
-# this will be my PyBank code
+# calculate the total number of months included in the datase
+# calculate the net total amount of "profit/losses" over the entire period
+# calculate the total amount of "profit/losses" over the entire period
+#   then find the average of those changes
+# calculate the greatest increase in profits (date and amount) ovetr the entire period
+# calculate the greatest decrease in profits (date and amount) ovetr the entire period
+#
+# for example:
+# Financial Analysis
+# ------------------
+# Total Months: 86
+# Total: $38382578
+# Average Change: $-2315.12
+# Greatest Increase in Profits: Feb-2012 ($1926159)
+# Greatest Decrease in Profits: Sep-2013 ($2196167)
+# 
+# The final script should both print the analysis to the terminal and export a text file with the results
+
+import os
+import csv
+
+budget_data = os.path.join("..", "Resources", "budget_data.csv")
+
+with open(budget_data, encoding='utf-8-sig') as csvfile:
+    csv_reader = csv.reader(csvfile.csv, delimiter=",")
+
+    # @NOTE: This time, we do not use `next(csv_reader)` because there is no header for this file
+
+    # Read through each row of data after the header
+    for row in csv_reader:
+
+        # Convert row to float and compare to grams of fiber
+        if float(row[1]) == "JAN-2010":
+            print(row)
+
+
