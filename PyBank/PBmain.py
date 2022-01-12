49f83bf4ceb541397dd714f5dec42646650b1e58
Pam Fernandez <ppossen@gmail.com>
2022-01-11 20:46:36 -0600

    commit

---
+++ PyBank/PBmain.py
@@ -0,0 +1,36 @@
+# calculate the total number of months included in the datase
+# calculate the net total amount of "profit/losses" over the entire period
+# calculate the total amount of "profit/losses" over the entire period
+#   then find the average of those changes
+# calculate the greatest increase in profits (date and amount) ovetr the entire period
+# calculate the greatest decrease in profits (date and amount) ovetr the entire period
+#
+# for example:
+# Financial Analysis
+# ------------------
+# Total Months: 86
+# Total: $38382578
+# Average Change: $-2315.12
+# Greatest Increase in Profits: Feb-2012 ($1926159)
+# Greatest Decrease in Profits: Sep-2013 ($2196167)
+# 
+# The final script should both print the analysis to the terminal and export a text file with the results
+
+import os
+import csv
+
+csvpath = os.path.join('..', 'PBResources', 'budget_data.csv')
+
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    print(csvreader)
+    
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
+
+    for row in csvreader:
+        print(row)
+
+
+
+
