3daea56e0828dac217367dbc6e6abe01958e2b1b
Pam Fernandez <ppossen@gmail.com>
2022-01-14 03:11:51 -0600

    how it started - the struggle

--- PyBank/PBmain.py
+++ PyBank/PBmain.py
@@ -19,18 +19,59 @@
 import os
 import csv
 
-csvpath = os.path.join('PBResources', 'budget_data.csv')
+# path to collect the data from the resources folder
+budget_csv = os.path.join('PBResources', 'budget_data.csv')
 
-with open(csvpath) as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=',')
-    print(csvreader)
+# following code from 3/Activities/08-Par_Wrestling/Solved
+# define the function and have it accept 'budgetdata' as its sole parameter
+def print_analysis(budgetdata):
+    total_months = str(budgetdata[0])
+    total = int(budgetdata[1])
+    avg_change = int(budgetdata[2])
+    great_inc = int(budgetdata[3])
+    great_dec = int(budgetdata[4])
+
+    # total_months can be found by summing 'Date'
+    total_months = sum(budget_csv[0])
+
+print(sum(total_months))
+
+#     # total can be found by summing "Profit/Losses"
+#     total_pl = sum('Profits/Losses')
+
+#     # avg_change can be found by dividing total_pl by total_months
+#     avg_change = (total_pl / total_months)
+
+#     # greatest increase can be found by 
+#     #great_inc =
+
+#     # greatest decreas can be found by
+#     #great_dec =
+
+#     # print the analysis
+#     print(f"Financial Analysis")
+#     print(f"------------------")
+#     print(f"Total Months: {total_months}")
+#     print(f"Total: {total_pl}")
+#     print(f"Average Change: {avg_change}")
+#     print(f"Greatest Increase in Profits: {great_inc}")
+#     print(f"Greatest Decrease in Profits: {great_dec}")
+
+# # following code from 3/Activities/01-Stu_CerealCleaner/Solved
+# # open and read csv
+# with open(budget_csv, 'r', encoding='utf-8') as csvfile:
+#     csvreader = csv.reader(csvfile, delimiter=',')
     
-    csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
+#     # read the header row first
+#     header = next(csvreader)
 
-    for row in csvreader:
-        print(row)
-
-
-
+# # print the analysis    
+    
+#     print(f"Financial Analysis")
+#     print(f"------------------")
+#     print(f"Total Months: {total_months}")
+#     print(f"Total: {total_pl}")
+#     print(f"Average Change: {avg_change}")
+#     print(f"Greatest Increase in Profits: {great_inc}")
+#     print(f"Greatest Decrease in Profits: {great_dec}")
 
