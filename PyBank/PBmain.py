9db72b2d73a9dc7ab0dd56c323fbd0ec1db291f8
Pam Fernandez <ppossen@gmail.com>
2022-01-15 19:22:24 -0600

    almost done code, minor errors

--- PyBank/PBmain.py
+++ PyBank/PBmain.py
@@ -1,7 +1,7 @@
-# calculate the total number of months included in the datase
+# calculate the total number of months included in the dataset
 # calculate the net total amount of "profit/losses" over the entire period
-# calculate the total amount of "profit/losses" over the entire period
-#   then find the average of those changes
+# calculate the changes in "profit/losses" over the entire period then find 
+#   the average of those changes
 # calculate the greatest increase in profits (date and amount) over the entire period
 # calculate the greatest decrease in profits (date and amount) over the entire period
 #
@@ -35,29 +35,29 @@ great_dec = 0
 great_dec_month = 0
 
 # open and read the CSV file
-with open(file_to_load, 'r') as budget_data:
-    reader = csv.reader(budget_data, delimiter=",")
+with open(file_to_load, 'r') as csvfile:
+    reader = csv.reader(csvfile, delimiter=",")
 
     # read the header row
-    header = next(reader)
+    header = next(csvfile)
+    
     # skip the header row
     # next(reader)
+    
     row = next(reader)
-    # Extract first row to avoid appending to net_change_list
-    first_row = next(reader)
-
-    # variables for calculations
+    
+    # variables for calculations 
     total_months += 1
-    profit_loss_total += int(first_row[1])
+    profit_loss_total += int(row[1])
     previous_row = int(row[1])
      
     # read each row of data
     for row in reader:
   
-        # total number of 'Dates' (months)
+        # calculate the total number of months included in the dataset
         total_months += 1
         
-        # total of all 'Profit/Losses'
+        # calculate the net total amount of "profit/losses" over the entire period
         profit_loss_total += int(row[1])
         
         # each months change - used only for calculating average change
@@ -65,12 +65,12 @@ with open(file_to_load, 'r') as budget_data:
         monthly_change.append(each_month_change)
         month_count.append(row[0])
         
-        # greatest increase
+        # calculate the greatest increase in profits (date and amount) over the entire period
         if int(row[1]) > great_inc:
             great_inc = int(row[1])
             great_inc_month = row[0]
        
-        # greatest decrease
+        # calculate the greatest decrease in profits (date and amount) over the entire period
         if int(row[1]) < great_dec:
             great_dec = int(row[1])
             great_dec_month = row[0]
@@ -87,13 +87,13 @@ with open(file_to_load, 'r') as budget_data:
 print(f"Financial Analysis")
 print(f"------------------")
 print(f"Total Months: {total_months}")
-print(f"Total: {profit_loss_total}")
+print(f"Total: ${profit_loss_total}")
 print(f"Average Change: ${avg_change}")
 print(f"Greatest Increase in Profits: {great_inc_month}, (${highest})")
 print(f"Greatest Decrease in Profits: {great_dec_month}, (${lowest})")
 
 
-# create output file
+# create output to a text file
 with open(file_to_output, 'w', newline='') as txtfile:
     txtfile.write(f"Financial Analysis\n")
     txtfile.write(f"------------------\n")
--- PyPoll/PPmain.py
+++ PyPoll/PPmain.py
@@ -28,35 +28,92 @@
 import csv
 import os
 
-# files to load and output
-file_to_load = os.path.join("PPResources", "election_data.csv")
-file_to_output = os.path.join("PBAnalysis", "election_analysis.txt")
-
 # variables
 total_votes = 0
-kahn_votes = 0
+khan_votes = 0
 correy_votes = 0
 li_votes = 0
 otooley_votes = 0
+other_votes = 0
+
+# file path
+csvpath = os.path.join("PPResources", "election_data.csv")
+file_to_output = os.path.join("PPAnalysis", "election_analysis.txt")
 
 # open and read the CSV file
-with open(file_to_load, 'r', newline='') as csvfile:
+with open(csvpath, "r", encoding='utf-8') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
 
-    # read the header row
-    header = next(csvfile)
     # skip the header row
-    # next(reader)
-    row = next(csvreader)
+    next(csvreader)
+    # iterate through the file
+    for row in csvreader:
 
-    # variables for calculations 
-
-    # read each row of data
-    for row in reader:
-
-        #calculate total number of votes
-        total_votes = total_votes + 1
-        print(total_votes)
+        # The total number of votes cast
+        # add one to the total_votes variable for each line in the reader
+        total_votes += +1
         
+        # A complete list of candidates who received votes        
+        # add to the candidate's _votes variable each time the specified candidate's name appears in csvdata
+        if (row[2] == "Khan"):
+            khan_votes += 1
+        elif(row[2] == "Correy"):
+            correy_votes += 1
+        elif(row[2] == "Li"):
+            li_votes += 1    
+        elif(row[2] == "O'Tooley"):
+            otooley_votes += 1
+        else:
+            other_votes += 1
+    
+    # The percentage of votes each candidate won
+    # (khan_votes / total_votes) - the times one-hundred part is in the output as ".3%"
+    khan_percent = (khan_votes / total_votes)
+    correy_percent = (correy_votes / total_votes)
+    li_percent = (li_votes / total_votes)
+    otooley_percent = (otooley_votes / total_votes)
 
+    # The winner of the election based on popular vote.
+    # make a list of the vote totals, winner is the one with the highest (max) number
+    winner_list = max(khan_votes, correy_votes, li_votes, otooley_votes, other_votes)
+    
+    if winner_list == khan_votes:
+        winner = "Khan"
+    if winner_list == correy_votes:
+        winner = "Correy"
+    if winner_list == li_votes:
+        winner = "Li"
+    if winner_list == otooley_votes:
+        winner = "O'Tooley"
+    else:
+        winner = "Other"
 
+# print analysis
+print("")
+print(f"Election Results")
+print(f"---------------------")
+print(f"Total Votes: {total_votes}")
+print(f"---------------------")
+print(f"Khan: {khan_percent:.3%}, {khan_votes}") 
+print(f"Correy: {correy_percent:.3%}, {correy_votes}") 
+print(f"Li: {li_percent:.3%}, {li_votes}") 
+print(f"O'Tooley: {otooley_percent:.3%}, {otooley_votes}") 
+print(f"---------------------")
+print(f"Winner: {winner}")
+print(f"---------------------")
+print("")
+
+# create output to a text file
+with open(file_to_output, 'w', newline='') as txtfile:
+    txtfile.write(f"Election Results\n")
+    txtfile.write(f"------------------------\n")
+    txtfile.write(f"Total Votes: {total_votes}\n")
+    txtfile.write(f"------------------------\n")
+    txtfile.write(f"Khan: {khan_percent:.3%}, {khan_votes}\n") 
+    txtfile.write(f"Correy: {correy_percent:.3%}, {correy_votes}\n") 
+    txtfile.write(f"Li: {li_percent:.3%}, {li_votes}\n") 
+    txtfile.write(f"O'Tooley: {otooley_percent:.3%}, {otooley_votes}\n") 
+    txtfile.write(f"------------------------\n")
+    txtfile.write(f"Winner: {winner}\n")
+    txtfile.write(f"------------------------\n")
+   
